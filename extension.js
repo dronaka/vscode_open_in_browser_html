6f5363407db5651572ba3ffb95b98c8757247f5a
dronaka <lucevar@yandex.ru>
2019-01-17 01:34:38 +0300

    generate standart template

---
+++ extension.js
@@ -0,0 +1,10 @@
+//import vscode
+const vscode = require('vscode');
+
+function activate(context) { }
+
+
+exports.activate = activate;
+
+function deactivate() { };
+exports.deactivate = deactivate;
