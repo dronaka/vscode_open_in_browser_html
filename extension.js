98004e3dc95bc2a037efaacfe85c66f28825e358
dronaka <lucevar@yandex.ru>
2019-01-17 02:56:57 +0300

    add main function and activation events

--- extension.js
+++ extension.js
@@ -1,7 +1,21 @@
 //import vscode
 const vscode = require('vscode');
+const opn = require('opn')
 
-function activate(context) { }
+function activate(context) {
+    const disposable = vscode.commands.registerCommand('extension.openWith', function () {
+        let editor = vscode.window.activeTextEditor
+        let doc = editor.document
+        vscode.window.showQuickPick(['chrome', 'yandex'], { placeHolder: 'Which app?' }).then((val) => {
+          if (val) {
+            opn(doc.fileName, { app: val })
+          } else {
+            return
+          }
+        })
+      })
+      context.subscriptions.push(disposable)
+ }
 
 
 exports.activate = activate;
